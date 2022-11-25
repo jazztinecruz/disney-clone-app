32011eaf3458fb74c9333af5a5630135de33689f
jazztine <jzztn.crz@gmail.com>
2022-11-25 13:39:26 +0800

    Fix bottom of sidebar menus

--- components/sidebar/menu.tsx
+++ components/sidebar/menu.tsx
@@ -7,7 +7,7 @@ const MenuIcon = ({ Icon, label }: Props) => {
   return (
     <div className="pl-10 py-6 w-full relative flex items-center opacity-50 hover:opacity-100 hover:scale-110 hover:font-bold transition-all duration-150">
       <Icon className="w-6 h-6 text-white" />
-      <span className="absolute left-20 bottom-4 hidden group-hover:block group-hover:animate-fade transition duration-100 text-white z-50 text-lg whitespace-nowrap">
+      <span className="absolute left-20 bottom-5 hidden group-hover:block group-hover:animate-fade transition duration-100 text-white z-50 text-lg whitespace-nowrap">
         {label}
       </span>
     </div>
