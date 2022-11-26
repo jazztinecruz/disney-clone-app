acadc45db072ddafaafd3c8653489780e9acb6d2
jazztine <jzztn.crz@gmail.com>
2022-11-26 16:25:51 +0800

    Modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        background: {
+          dark: "#101115",
+        },
+        typography: {
+          light: "#D3D3D4",
+        },
+        card: {
+          dark: "#1C1F2A",
+        },
+        action: {
+          dark: "#222326",
+        },
+        rated: {
+          dark: "#3F4043",
+        },
+      },
       animation: {
-        "fade": "fade 0.3s ease-in",
+        fade: "fade 0.3s ease-in",
       },
       keyframes: {
         fade: {
