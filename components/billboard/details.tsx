976aa802b5b44f56652b800d76e263d56525fb2c
jazztine <jzztn.crz@gmail.com>
2022-11-29 23:48:02 +0800

    Modify billboard details font size

--- components/billboard/details.tsx
+++ components/billboard/details.tsx
@@ -3,7 +3,7 @@ import Button from "../Button";
 
 const Details = () => {
   return (
-    <div className="absolute -top-80 left-0 right-0 max-w-lg flex flex-col gap-6 z-[100]">
+    <div className="absolute -top-80 left-0 right-0 max-w-lg flex flex-col gap-4 z-[100]">
       <span className="text-6xl font-bold">LIGHTYEAR</span>
       <div className="font-semibold tracking-wider text-white/75 flex items-center gap-3">
         <span>2022</span>
@@ -17,22 +17,22 @@ const Details = () => {
         </span>
       </div>
 
-      <p className="text-md font-semibold">
+      <p className="font-semibold">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
         tenetur?
       </p>
 
-      <div className="text-md font-semibold flex items-center gap-3">
+      <div className="font-semibold flex items-center gap-3">
         <span>Fantasy</span>
         <div>|</div>
         <span>Adventure</span>
       </div>
 
-      <div className="w-full grid grid-cols-[1fr,auto] gap-6  items-center">
+      <div className="w-full grid grid-cols-[1fr,auto] gap-6 items-center mt-6">
         <Button>
           <div className="flex items-center gap-3">
             <PlayIcon className="w-6 h-6" />
-            <span className="font-semibold tracking-wide whitespace-nowrap ">
+            <span className="font-bold tracking-wide whitespace-nowrap ">
               Watch Now
             </span>
           </div>
