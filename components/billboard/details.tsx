94f1093aeba065ef58f75c6f837877733c88a95b
jazztine <jzztn.crz@gmail.com>
2022-11-29 23:41:58 +0800

    Modify billboard  details

--- components/billboard/details.tsx
+++ components/billboard/details.tsx
@@ -3,8 +3,8 @@ import Button from "../Button";
 
 const Details = () => {
   return (
-    <div className="max-w-lg border-4 flex flex-col gap-6">
-      <span className="text-6xl  font-bold">LIGHTYEAR</span>
+    <div className="absolute -top-80 left-0 right-0 max-w-lg flex flex-col gap-6 z-[100]">
+      <span className="text-6xl font-bold">LIGHTYEAR</span>
       <div className="font-semibold tracking-wider text-white/75 flex items-center gap-3">
         <span>2022</span>
         <div className="w-1 h-1 bg-white/50 rounded-full"></div>
@@ -12,7 +12,7 @@ const Details = () => {
         <div className="w-1 h-1 bg-white/50 rounded-full"></div>
         <span>English</span>
         <div className="w-1 h-1 bg-white/50 rounded-full"></div>
-        <span className="w-8 h-7 bg-white/20 grid place-items-center rounded-sm">
+        <span className="w-8 h-7 bg-rated-dark grid place-items-center rounded-sm">
           <span>PG</span>
         </span>
       </div>
@@ -31,13 +31,15 @@ const Details = () => {
       <div className="w-full grid grid-cols-[1fr,auto] gap-6  items-center">
         <Button>
           <div className="flex items-center gap-3">
-            <PlayIcon className="w-6 h-6 text-white" />
-            <span className="font-semibold tracking-wide whitespace-nowrap">Watch Now</span>
+            <PlayIcon className="w-6 h-6" />
+            <span className="font-semibold tracking-wide whitespace-nowrap ">
+              Watch Now
+            </span>
           </div>
         </Button>
 
         <Button>
-          <PlusIcon className="w-8 h-8 text-white" />
+          <PlusIcon className="w-8 h-8" />
         </Button>
       </div>
     </div>
