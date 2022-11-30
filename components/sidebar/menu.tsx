78d22d649e4d70f5bbee57499a2cb600a52d3e12
jazztine <jzztn.crz@gmail.com>
2022-11-30 22:40:49 +0800

    Modify sidebar on mobile

--- components/sidebar/menu.tsx
+++ components/sidebar/menu.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const MenuIcon = ({ Icon, label }: Props) => {
   return (
-    <div className="pl-10 py-6 w-full relative flex items-center opacity-50 hover:opacity-100 hover:scale-110 hover:font-bold transition-all duration-150">
+    <div className="pl-8 md:pl-10 py-6 w-full relative flex items-center opacity-50 hover:opacity-100 hover:scale-110 hover:font-bold transition-all duration-150">
       <Icon className="w-6 h-6 text-white" />
       <span className="absolute left-20 bottom-5 hidden group-hover:block group-hover:animate-fade transition duration-500 ease-out text-white z-50 text-lg whitespace-nowrap">
         {label}
